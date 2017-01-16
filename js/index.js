ed9a803ec7f428f3b492f3490ab01289e2076db2
minessilva <mariainesssts@gmail.com>
2017-01-16 19:40:17 +0000

    Add: folders

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//          Inspitation page:
+
+//https://www.google.com/design/spec/animation/responsive-interaction.html#responsive-interaction-material-response
+
+//          #3 Burger Menu...
+//codepen.io/victorfreire/pen/pgjeOy
+
+//https://github.com/VictorFreire/2-Material-Design-Burger-Menu
